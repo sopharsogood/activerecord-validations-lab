db567464fa729ab8cdea60c9cf195b57bfcd8b2e
sophie <justanothersophie@gmail.com>
2021-09-08 16:33:45 -0400

    commit tree 78733c1d115f55c56b0bcc7f2964910e08c3c572

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    validate :title_must_be_clickbait
+
+    def title_must_be_clickbait
+        if !title
+        elsif !title.match?(/Won't Believe|Secret|Top.\d|Guess/)
+            errors.add(:title, "must be clickbait")
+            puts "Oh no!"
+        end
+    end
+        
 end
